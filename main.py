1232d1e35c0e52043496c84427b81b3a75974837
JRizzly <JRizzly@gmail.com>
2018-04-01 16:29:44 -0400

    latestPush
    Has most of the Framework, Needs to modifiy assignemnt of highest scored
    State, and needs the actual accurate MOMs in attack functions.

--- main.py
+++ main.py
@@ -15,10 +15,12 @@ import datetime
 import time
 import re
 import os.path
+import copy
 import random
 from math import radians, cos, sin, asin, sqrt
 
-
+global inf
+inf = 999999999.9
 
 #global firstTime
 #firstTime = 0
@@ -68,7 +70,7 @@ class Location():
 
 
 class Munition():
-    def __init__(self, name, platform, cost, range, cep, reliability, quantity):
+    def __init__(self, name, platform, cost, range, cep, reliability, quantity, blastRadius):
         self.Name = name
         self.Platform   = platform
         self.Cost = cost
@@ -76,10 +78,7 @@ class Munition():
         self.Cep = cep
         self.Reliability = reliability
         self.Quantity = quantity
-
-
-
-
+        self.BlastRadius = blastRadius
 
 
 
@@ -95,17 +94,16 @@ class Target():
         self.inRangeof = []
 
         #for quantifying effectiveness
-             #operational Effectiveness
-        self.NumAirCraftLost = 0
-        self.TypeMunitionUsed = None
-        self.NumMunitionsUsed = 0
+        #operational Effectiveness
+        self.NumAirCraftLost = 0.0
+        self.MunitionsUsed = []
 
              #Cost of Stategy
-        self.MunitionCost = 0
-        self.DownedAirCraftCost = 0
+        self.MunitionCost = 0.0
+        self.DownedAirCraftCost = 0.0
 
-             #Collateral Damage
-        self.NumOfCasualties = 0
+        #Collateral Damage
+        self.NumOfCasulties = 0.0
 
 
 
@@ -122,41 +120,65 @@ class Target():
         print self.whoAssigned.printt()
 
 
-
 class Solution():
-    def __init__(self):
-        self.Targets = []
+    def __init__(self, targets):
+        #array of destroyed targets
+        self.Targets = targets
 
+        # ------------- Totals -------
+        #Ops Effectivness
+        self.TotNumAirCraftLost = 0.0
+        self.TotMunitionsUsed = []
+        #Cost
+        self.TotMunitionCost = 0.0
+        self.TotDownedAirCraftCost = 0.0
+        #Collateral Damage
+        self.TotNumOfCasualties = 0.0
 
-        self.OperationalEffectivenessScore = 0
-        self.CostScore = 0
-        self.CollateralDamageScore = 0
-        self.TotalScore = 0
-        self.WeightedScore = 0
+        #------------- Scores -------------------
+        self.OperationalEffectivenessScore = 0.0
+        self.CostScore = 0.0
+        self.CollateralDamageScore = 0.0
+        self.TotalScore = 0.0
+        self.WeightedScore = 0.0
 
     def calculateScore(self):
         for t in range(0, len(self.Targets)):
-            # for quantifying effectiveness
-            # operational Effectiveness
-            self.NumAirCraftLost = 0
-            self.TypeMunitionUsed = None
-            self.NumMunitionsUsed = 0
+            self.TotNumAirCraftLost = self.TotNumAirCraftLost + self.Targets[t].NumAirCraftLost
+            self.TotMunitionsUsed = self.TotMunitionsUsed + self.Targets[t].MunitionsUsed
+            self.TotMunitionCost += self.Targets[t].MunitionCost
+            self.TotDownedAirCraftCost += self.Targets[t].DownedAirCraftCost
+            self.TotNumOfCasualties += self.Targets[t].NumOfCasulties
 
-            # Cost of Stategy
-            self.MunitionCost = 0
-            self.DownedAirCraftCost = 0
+        #Building Scores
+        # I think we also need to assign weighting to the factors which make up a MOM
+        #Right now it weights the use of 1 munition the same as the loss of an aircraft
+        self.OperationalEffectivenessScore = self.TotNumAirCraftLost + len( self.TotMunitionsUsed )
+        self.CostScore = self.TotMunitionCost + self.TotDownedAirCraftCost
+        self.CollateralDamageScore = self.TotNumOfCasualties
 
-            # Collateral Damage
-            self.NumOfCasualties = 0
+        #TODO: make low cost better the score simple subtraction right now
+        self.TotalScore = self.OperationalEffectivenessScore*float(.55) - self.CostScore*float(.1) + self.CollateralDamageScore*float(.35)
+        self.WeightedScore = self.TotalScore/len(self.Targets)
+
+        print "Total Cost: " + str(self.TotMunitionCost)
 
 
+
+class Family():
+    def __init__(self):
+        self.children = []
 
 
 class Scenario():
-    def __init__(self, targets, units):
+    def __init__(self, targets, units, munitions, age):
         self.Targets = targets
         self.Units = units
+        self.Munitions = munitions
         self.Solution = None
+        self.SolutionSets = []
+        self.Age = age
+        self.PreviousCleanTargetState = None
 
     def findRandomAirForceUnitLocation(self):
         found = False
@@ -199,8 +221,13 @@ class Scenario():
                     self.Targets[t].whoAssigned = self.Units[self.findRandomUnitLocation()]
                     self.Targets[t].Assigned = True
 
+        #setting Previous state for clean targets with assignemnts
+        self.PreviousCleanTargetState = copy.deepcopy(self.Targets)
+
         for t in range(0, len(self.Targets)):
             print self.Targets[t].printt()
+
+
 
 
     def AFAttack(self,t):
@@ -208,6 +235,11 @@ class Scenario():
         #target at this specific location
 
         '''Complicated Math here '''
+
+        self.Targets[t].MunitionCost += 1.0
+
+
+
 
         #It will end with target being destroyed
         self.Targets[t].Destroyed = True
@@ -219,6 +251,8 @@ class Scenario():
 
         '''Complicated Math here '''
 
+        self.Targets[t].MunitionCost += 10.0
+
         #It will end with target being destroyed
         self.Targets[t].Destroyed = True
         self.MunitionUsed = "Munition that best fits our equation"
@@ -228,6 +262,7 @@ class Scenario():
         #target at this specific location
 
         '''Complicated Math here '''
+        self.Targets[t].MunitionCost += 100.0
 
         #It will end with target being destroyed
         self.Targets[t].Destroyed = True
@@ -239,40 +274,94 @@ class Scenario():
         # for all targets
         for t in range(0, len(self.Targets)):
             #attack modeld by type of uint
-            if ( self.Targets.whoAssigned.Type == 1 ):
+            if ( self.Targets[t].whoAssigned.Type == 1 ):
                 self.AFAttack(t)
-            if (self.Targets.whoAssigned.Type == 2):
+            if (self.Targets[t].whoAssigned.Type == 2):
                 self.ArmyAttack(t)
-            if ( self.Targets.whoAssigned.Type == 3 ):
+            if ( self.Targets[t].whoAssigned.Type == 3 ):
                 self.NavyAttack(t)
 
 
+    def checkAndCalculateScore(self):
+        AssignedAndDestroyed = True
+        count = 0
+        solution1 = Solution(self.Targets)
+        for t in range(0, len(self.Targets)):
+            if ( self.Targets[t].Assigned == False or self.Targets[t].Destroyed == False  ):
+                AssignedAndDestroyed = False
+                count += 1
+
+        if ( AssignedAndDestroyed == True  ) :
+            solution1.calculateScore()
+            self.Solution = copy.deepcopy(solution1)
+            self.SolutionSets.append(copy.deepcopy(self.Solution))
+        else:
+            print "Not All Targets are Assigned or Destoryed: " + str(count ) + " Not Assigned or Destroyed"
 
 
-
-
-    def runSim(self):
-
-        #update all targets with units they can be hit by
+    def setFeasibleTargest(self):
+        # update all targets with units they can be hit by with respect to unit Grand Range
         for t in range(0, len(self.Targets)):
 
             for i in range(0, len(self.Units)):
-                if distance(self.Targets[t].Location, self.Units[i].Location) < self.Units[i].Range :
+                if distance(self.Targets[t].Location, self.Units[i].Location) < self.Units[i].Range:
                     self.Targets[t].inRangeof.append(self.Units[i])
 
-            #print self.Targets[t].printt()
+            # print self.Targets[t].printt()
 
+
+    def modifyAssignments(self):
+        #Randomly change an assignment
+
+        randTarget = random.randint(0, len(self.Targets) - 1)
+
+        # for all units in range of this target
+        for i in range(0, len(self.Targets[randTarget].inRangeof)):
+
+            # if Mobile Target then Assign Random Air Force Unit
+            if self.Targets[randTarget].Mobile == True:
+                self.Targets[randTarget].whoAssigned = self.Units[self.findRandomAirForceUnitLocation()]
+                self.Targets[randTarget].Assigned = True
+
+            # assign random Unit to  target
+            if self.Targets[randTarget].Assigned == True:
+                self.Targets[randTarget].whoAssigned = self.Units[self.findRandomUnitLocation()]
+                self.Targets[randTarget].Assigned = True
+
+
+    def resetToPreviousCleanState(self):
+        self.Targets = copy.deepcopy(self.PreviousCleanTargetState)
+        self.Solution = None
+
+    def runSim(self):
+
+        #sets Targets based on Grand Ranges
+        self.setFeasibleTargest()
 
         #first time - Random assignment
         self.firstTimeRandomAssignments()
+
+        #setting previous State
+        #self.PreviousState = copy.deepcopy(self.Targets)
 
         #Simulate Attacks
         self.simulateAttacks()
 
         #Calculate Score after attacks
-        self.calculateScore()
+        self.checkAndCalculateScore()
 
-        #Modify Assignments
+        for i in range(0, self.Age):
+            #Load Previous State
+            self.resetToPreviousCleanState()
+
+            #Modify Assignments
+            self.modifyAssignments()
+
+            # Simulate Attacks
+            self.simulateAttacks()
+
+            # Calculate Score after attacks
+            self.checkAndCalculateScore()
 
 
 
@@ -311,9 +400,14 @@ def distance(Location1, Location2):
 
 
 def main():
+    global inf
     #Read in from files
     #f = open('OfficialRates2.csv', 'a', 0)
 
+    #Service Assignments
+    #1 - air force , 2 is army , 3 is navy
+
+    ###### UNITS #####
     #Colorado SPrings
     loc1 = Location(38.8058, -104.7008)
     unit1 = Unit(loc1,999999,1, "AFJohnny")
@@ -326,6 +420,7 @@ def main():
     loc3 = Location(28.11787, -95.37432)
     unit3 = Unit(loc3, 1126.0, 3, "Navy Ned")
 
+    ###### Targets #####
     #Dallas Targets
     loc4 = Location(32.7584072, -96.7359924)
     loc5 = Location(33.7584072, -95.7359924)
@@ -334,18 +429,44 @@ def main():
     target2 = Target(loc5, False)
     target3 = Target(loc6, False)
 
+    ###### Munitions #####
+    #Distance all in km and and blast radius in meters
+    # def __init__(self, name, platform, cost, range, cep, reliability, quantity, blastRadius):
+    mun1 = Munition("GBU-10", 1, 23000.0, inf, 9, .95, inf, 20)
+    mun2 = Munition("ATACM Block IVA",2, 820000.0, 300.0, 4.0, .92,96, 100.0)
+    mun3 = Munition("USN DDG",3,500000.0,1111.2,12.2,.85,inf, 10)
+
+
+
+
+    #TODO: Handle the case switching assignments in the case of no muni8tions left to be used by that unit
 
     #Initialize and Load Scenario from files
+    grandFamily = Family()
+    numKids = 1
+    childAge = 3
 
-    world =  Scenario([target1, target2,target3], [unit1, unit2, unit3])
+    #build the number of Children in the family
+    for i in range(0, numKids):
+        child = Scenario([target1, target2,target3], [unit1, unit2, unit3], [mun1, mun2, mun3], childAge)
+        grandFamily.children.append(copy.deepcopy(child))
 
-    #Run Simulation
+    #Run evolution of each child
+    # seperate thread so it can be multithreaded as needed
+    for i in range(0, len(grandFamily.children )):
+        grandFamily.children[i].runSim()
+        print "End Scenario "
+        print ""
+
+
+    '''
+    for i in range(0, children):
+        world =  Scenario([target1, target2,target3], [unit1, unit2, unit3], [mun1, mun2, mun3])
+
+    #One Child and Grows
     world.runSim()
+    '''
 
-    #Evaluate Solution
-    #world.evaluateSolution
-
-    #loop for improvement
 
 
 
